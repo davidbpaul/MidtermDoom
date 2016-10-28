66bbeab9f99e17db18e3840e0810bbc86ac22951
Noelle <perdue.noelle@gmail.com>
2016-10-28 20:41:24 +0000

    Friday commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,145 +9,68 @@ $(() => {
   });
 });
 
-;(function ($, window, document, undefined) {
-    var pluginName = 'pinterest_grid',
-        defaults = {
-            padding_x: 10,
-            padding_y: 10,
-            no_columns: 3,
-            margin_bottom: 50,
-            single_column_breakpoint: 700
-        },
-        columns,
-        $article,
-        article_width;
+$(function() {
+  function renderReference(references) {
+    $('#tweets-container').empty();
+    $.each(tweets, function(index, ref) {
+      var $eachref = createRefElement(ref);
+      $eachref.prependTo("#reference-container");
+    });
 
-    function Plugin(element, options) {
-        this.element = element;
-        this.options = $.extend({}, defaults, options) ;
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.init();
-    }
+  }
 
-    Plugin.prototype.init = function () {
-        var self = this,
-            resize_finish;
+  function createReferenceElement(reference) {
+    console.log(tweet);
+    var $tweet = $('<article>').addClass('tweet');
+    var time = new Date(tweet.created_at)
+    var formatTime = time.toLocaleDateString();
+    $reference.html(
+    `<article class = "white-panel">
+         <header class = "header_is_on_top">
+           <div class = "rating_score">
+             <h3 class = "rating">5</h3>
+           </div>
+           <div class = "title"><h6><small>something I found interesting online</small></h6></div>
+         </header>
+         <section class= "body">
+           <div class ='article_picture'>
+             <img src="http://i.imgur.com/sDLIAZD.png" alt="">
+           </div>
+           <div class = 'article_title'>
+             <h4><a href="#">Title 1</a></h4>
+           </div>
+         <div class = 'description'>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+           irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+         </div>
+         </section>
+         <section>
+            <div class = "head">
+                <a class = "like"><big>like</big></a>
+            </div>
+         </section>
+           <section class = "foot">
+             <p><small>12 people have liked this..</small>
+         </section>
+             </article>`)
+    return $tweet;
+  }
 
-        $(window).resize(function() {
-            clearTimeout(resize_finish);
-            resize_finish = setTimeout( function () {
-                self.make_layout_change(self);
-            }, 11);
-        });
+  $("#submitTweet").on("click", function(event) {
+    event.preventDefault();
 
-        self.make_layout_change(self);
+    $.ajax({
+      method: 'POST',
+      url: "/references",
+      data: $('#tweetForm').serialize(),
+      success: function(tweets) {
+        redirect("/myref");
+      }
 
-        setTimeout(function() {
-            $(window).resize();
-        }, 500);
-    };
-
-    Plugin.prototype.calculate = function (single_column_mode) {
-        var self = this,
-            tallest = 0,
-            row = 0,
-            $container = $(this.element),
-            container_width = $container.width();
-            $article = $(this.element).children();
-
-        if(single_column_mode === true) {
-            article_width = $container.width() - self.options.padding_x;
-        } else {
-            article_width = ($container.width() - self.options.padding_x * self.options.no_columns) / self.options.no_columns;
-        }
-
-        $article.each(function() {
-            $(this).css('width', article_width);
-        });
-
-        columns = self.options.no_columns;
-
-        $article.each(function(index) {
-            var current_column,
-                left_out = 0,
-                top = 0,
-                $this = $(this),
-                prevAll = $this.prevAll(),
-                tallest = 0;
-
-            if(single_column_mode === false) {
-                current_column = (index % columns);
-            } else {
-                current_column = 0;
-            }
-
-            for(var t = 0; t < columns; t++) {
-                $this.removeClass('c'+t);
-            }
-
-            if(index % columns === 0) {
-                row++;
-            }
-
-            $this.addClass('c' + current_column);
-            $this.addClass('r' + row);
-
-            prevAll.each(function(index) {
-                if($(this).hasClass('c' + current_column)) {
-                    top += $(this).outerHeight() + self.options.padding_y;
-                }
-            });
-
-            if(single_column_mode === true) {
-                left_out = 0;
-            } else {
-                left_out = (index % columns) * (article_width + self.options.padding_x);
-            }
-
-            $this.css({
-                'left': left_out,
-                'top' : top
-            });
-        });
-
-        this.tallest($container);
-        $(window).resize();
-    };
-
-    Plugin.prototype.tallest = function (_container) {
-        var column_heights = [],
-            largest = 0;
-
-        for(var z = 0; z < columns; z++) {
-            var temp_height = 0;
-            _container.find('.c'+z).each(function() {
-                temp_height += $(this).outerHeight();
-            });
-            column_heights[z] = temp_height;
-        }
-
-        largest = Math.max.apply(Math, column_heights);
-        _container.css('height', largest + (this.options.padding_y + this.options.margin_bottom));
-    };
-
-    Plugin.prototype.make_layout_change = function (_self) {
-        if($(window).width() < _self.options.single_column_breakpoint) {
-            _self.calculate(true);
-        } else {
-            _self.calculate(false);
-        }
-    };
-
-    $.fn[pluginName] = function (options) {
-        return this.each(function () {
-            if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName,
-                new Plugin(this, options));
-            }
-        });
-    }
-
-})(jQuery, window, document);
+    });
+     $('textarea.text').val == "";
+   });
+  }
+)
 
 
