b6c4b29495d9725547329a10f11a1991a908d7e3
Noelle <perdue.noelle@gmail.com>
2016-10-29 23:18:18 +0000

    mmMMMcommit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,44 +19,75 @@ $(() => {
 
   }
 
-function createReferenceElement(ref) {
-    console.log(ref);
-    var $reference = $('<article>').addClass('ref');
-    var time = new Date(ref.created_at)
-    var formatTime = time.toLocaleDateString();
-    $reference.html(
+  const createReferenceElement = (ref) => {
+    let $resource = $("<article>").addClass('white-panel').appendTo("section#pinBoot");
+    $resource.append(`<header class = "header_is_on_top">
+                        <div class = "rating_score">
+                          <h3 class = "rating">5</h3>
+                        </div>
+                        <div class = "title"><h6><small>something I found interesting online</small></h6></div>
+                      </header>
+                      <section class= "body">
+                        <div class ='article_picture'>
+                          <img src="http://i.imgur.com/sDLIAZD.png" alt="">
+                        </div>
+                        <div class = 'article_title'>
+                          <h4><a href="#">Title 1</a></h4>
+                        </div>
+                        <div class = 'description'>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim venerit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                      </section>
+                      <section>
+                         <div class = "head">
+                             <a class = "like"><big>like</big></a>
+                         </div>
+                      </section>
+                      <section class = "foot">
+                        <p><small>12 people have liked this..</small>
+                      </section>`)
+    return $resource;
+    }
+  })
 
-      `<article class = "white-panel">
-          <header class = "header_is_on_top">
-            <div class = "rating_score">
-              <h3 class = "rating">5</h3>
-            </div>
+// function createReferenceElement(ref) {
+//     console.log(ref);
+//     var $reference = $('<article>').addClass('ref');
+//     var time = new Date(ref.created_at)
+//     var formatTime = time.toLocaleDateString();
+//     $reference.html(
 
-            <div class = "title"><h6><small>something I found interesting online</small></h6></div>
-          </header>
-          <section class= "body">
-            <div class ='article_picture'>
-              <img src="http://i.imgur.com/sDLIAZD.png" alt="">
-            </div>
-            <div class = 'article_title'>
-              <h4><a href="#">Title 1</a></h4>
-            </div>
-          <div class = 'description'>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-          </section>
-          <section>
-             <div class = "head">
-                 <a class = "like"><big>like</big></a>
-             </div>
-          </section>
-            <section class = "foot">
-              <p><small>12 people have liked this..</small>
-          </section>
-              </article>`)
-    return $reference;
-  }
+//       `<article class = "white-panel">
+//           <header class = "header_is_on_top">
+//             <div class = "rating_score">
+//               <h3 class = "rating">5</h3>
+//             </div>
+
+//             <div class = "title"><h6><small>something I found interesting online</small></h6></div>
+//           </header>
+//           <section class= "body">
+//             <div class ='article_picture'>
+//               <img src="http://i.imgur.com/sDLIAZD.png" alt="">
+//             </div>
+//             <div class = 'article_title'>
+//               <h4><a href="#">Title 1</a></h4>
+//             </div>
+//           <div class = 'description'>
+//             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+//             irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+//           </div>
+//           </section>
+//           <section>
+//              <div class = "head">
+//                  <a class = "like"><big>like</big></a>
+//              </div>
+//           </section>
+//             <section class = "foot">
+//               <p><small>12 people have liked this..</small>
+//           </section>
+//               </article>`)
+//     return $reference;
+//   }
 
 function loadReference() {
     $.ajax({
