ac0bc97ee905ef5c5911629a5b1ea684ab23430a
vagrant <vagrant@lighthouse-labs>
2016-10-29 17:00:16 +0000

    non3d

--- public/scripts/bootstrap.js
+++ public/scripts/bootstrap.js
@@ -1,12 +1,12 @@
-   $(document).ready(function() {
-    $('#pinBoot').pinterest_grid({
+$(document).ready(function() {
+  $('#pinBoot').pinterest_grid({
     no_columns: 4,
     padding_x: 10,
     padding_y: 10,
     margin_bottom: 50,
     single_column_breakpoint: 700
     });
-    });
+  });
 
   /*
   Ref:
@@ -32,7 +32,7 @@
 
 
   */
-    ;(function ($, window, document, undefined) {
+  ;(function ($, window, document, undefined) {
       var pluginName = 'pinterest_grid',
         defaults = {
           padding_x: 10,
@@ -45,68 +45,61 @@
         $article,
         article_width;
 
-      function Plugin(element, options) {
-          this.element = element;
-          this.options = $.extend({}, defaults, options) ;
-          this._defaults = defaults;
-          this._name = pluginName;
-          this.init();
-      }
+    function Plugin(element, options) {
+      this.element = element;
+      this.options = $.extend({}, defaults, options) ;
+      this._defaults = defaults;
+      this._name = pluginName;
+      this.init();
+    }
 
-      Plugin.prototype.init = function () {
-        var self = this,
-          resize_finish;
-          $(window).resize(function() {
-              clearTimeout(resize_finish);
-              resize_finish = setTimeout( function () {
-                self.make_layout_change(self);
-              }, 11);
-          });
+  Plugin.prototype.init = function () {
+    var self = this,
+      resize_finish;
+      $(window).resize(function() {
+        clearTimeout(resize_finish);
+        resize_finish = setTimeout( function () {
           self.make_layout_change(self);
-          setTimeout(function() {
-            $(window).resize();
-          }, 500);
-      };
-      Plugin.prototype.calculate = function (single_column_mode) {
-          var self = this,
-              tallest = 0,
-              row = 0,
-              $container = $(this.element),
-              container_width = $container.width();
-              $article = $(this.element).children();
-
-          if(single_column_mode === true) {
-              article_width = $container.width() - self.options.padding_x;
+        }, 11);
+      });
+      self.make_layout_change(self);
+      setTimeout(function() {
+        $(window).resize();
+      }, 500);
+  };
+  Plugin.prototype.calculate = function (single_column_mode) {
+    var self = this,
+      tallest = 0,
+      row = 0,
+      $container = $(this.element),
+      container_width = $container.width();
+      $article = $(this.element).children();
+      if(single_column_mode === true) {
+        article_width = $container.width() - self.options.padding_x;
+      } else {
+        article_width = ($container.width() - self.options.padding_x * self.options.no_columns) / self.options.no_columns;
+      }
+      $article.each(function() {
+        $(this).css('width', article_width);
+      });
+      columns = self.options.no_columns;
+      $article.each(function(index) {
+        var current_column,
+          left_out = 0,
+          top = 0,
+          $this = $(this),
+          prevAll = $this.prevAll(),
+          tallest = 0;
+          if(single_column_mode === false) {
+            current_column = (index % columns);
           } else {
-              article_width = ($container.width() - self.options.padding_x * self.options.no_columns) / self.options.no_columns;
+            current_column = 0;
           }
-
-          $article.each(function() {
-              $(this).css('width', article_width);
-          });
-
-          columns = self.options.no_columns;
-
-          $article.each(function(index) {
-              var current_column,
-                  left_out = 0,
-                  top = 0,
-                  $this = $(this),
-                  prevAll = $this.prevAll(),
-                  tallest = 0;
-
-              if(single_column_mode === false) {
-                  current_column = (index % columns);
-              } else {
-                  current_column = 0;
-              }
-
-              for(var t = 0; t < columns; t++) {
-                  $this.removeClass('c'+t);
-              }
-
-              if(index % columns === 0) {
-                  row++;
+          for(var t = 0; t < columns; t++) {
+            $this.removeClass('c'+t);
+          }
+          if(index % columns === 0) {
+            row++;
               }
 
               $this.addClass('c' + current_column);
