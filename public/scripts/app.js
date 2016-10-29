a12aff253c492c3c6faff3408f199470a9a25910
vagrant <vagrant@lighthouse-labs>
2016-10-29 22:42:23 +0000

    w

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,92 +1,74 @@
-// // img url unvalid
-
-// $(() => {
-//   function renderRef(references) {
-//     $('#references-container').empty();
-//     $.each(references, function(index, ref) {
-//       console.log(ref);
-//       var $eachref = createReferenceElement(ref);
-//       $eachref.prependTo("#references-container");
-//     });
-
-//   }
-
-// function createReferenceElement(ref) {
-//     console.log(ref);
-//     var $reference = $('<article>').addClass('ref');
-//     var time = new Date(ref.created_at)
-//     var formatTime = time.toLocaleDateString();
-//     $reference.html(
-
-//       `<article class = "white-panel">
-//           <header class = "header_is_on_top">
-//             <div class = "rating_score">
-//               <h3 class = "rating">${ref.ratings.rating}</h3>
-//             </div>
-
-//             <div class = "title">
-//               <h6><small>${ref.users.name}</small></h6></div>
-//           </header>
-//           <section class= "body">
-//             <div class ='article_picture'>
-//               <img src="${ref.resources.img_url}" alt="">
-//             </div>
-//             <div class = 'article_title'>
-//               <h4><a href="#${ref.resources.link}">${ref.resources.title}</a></h4>
-//             </div>
-//           <div class = 'description'>
-//             <p>${ref.resources.description}</p>
-//           </div>
-//           </section>
-//           <section>
-//              <div class = "head">
-//                  <a class = "like"><big>like</big></a>
-//              </div>
-//           </section>
-//             <section class = "foot">
-//               <p><small>${people_} people have liked this..</small>
-//           </section>
-//               </article>`)
-//     return $reference;
-//   }
-
-// function loadReference() {
-//     $.ajax({
-//       method: 'GET',
-//       url: '/',
-//       dataType: 'json',
-//       success: function(references) {
-//         renderRef(references);
-
-//       }
-//     });
-//   }
-
-// loadReference();
-
-// $("#submitReference").on("click", function(event) {
-//     event.preventDefault();
-//     if ($('textarea.text').val().length == 0) {
-//       return;
-//     }
-//     $.ajax({
-//       method: 'POST',
-//       url: "/",
-//       data: $('#referenceForm').serialize(),
-//       success: function(ref) {
-//         loadReference();
-//       }
-
-//     });
-//      $('textarea.text').val == "";
-
-// });
-
-
-
-
-// // may need to hide compose reference
-
-
 // // database
+$(document).ready(function(){
 
+ function renderRef(references) {
+   $('#references-container').empty();
+   $.each(references, function(index, ref) {
+     console.log(ref);
+     var $eachref = createArticleElement(ref);
+     $eachref.prependTo("#references-container");
+   });
+
+ }
+
+
+  function createArticleElement(article) {
+
+    const articlecontent = `<article class = "white-panel">
+                         <header class = "header_is_on_top">
+                        <div class = "rating_score">
+                        <h3 class = "rating">5</h3>
+              </div>
+              <div class = "title"><h6><small>something I found interesting online</small></h6></div>
+            </header>
+            <section class= "body">
+              <div class ='article_picture'>
+                <img src="http://i.imgur.com/sDLIAZD.png" alt="">
+              </div>
+              <div class = 'article_title'>
+                <h4><a href="#">Title 1</a></h4>
+              </div>
+              <div class = 'description'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+            </section>
+            <section>
+               <div class = "head">
+                   <a class = "like"><big>like</big></a>
+               </div>
+            </section>
+            <section class = "foot">
+              <p><small>12 people have liked this..</small>
+            </section>
+          </article>`
+    return articlecontent;
+  };
+ loadArticle()
+
+  $("#referenceForm").on("submit", function (ev) {
+    ev.preventDefault();
+    const text = $('#referenceForm textarea').val()
+    const data = $(this).serialize()
+    console.log(data)
+      $.ajax({
+        url: '/api/resources',
+        method: 'POST',
+        data: data,
+        success: loadArticle(data)
+      });
+
+      $('#referenceForm textarea').val("")
+  });
+
+  function loadArticle(data){
+    $.ajax({
+      url: '/api/resources',
+      method: 'GET',
+      success: function (data) {
+        renderArticles(data);
+      }
+    })
+  }
+  loadArticle();
+});
