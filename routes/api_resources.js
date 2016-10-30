ece4ae1daf6462f1a7d4e60af16be7d7aa4a752e
Noelle <perdue.noelle@gmail.com>
2016-10-30 19:07:36 +0000

    Sunday early

--- routes/api_resources.js
+++ routes/api_resources.js
@@ -79,5 +79,29 @@ module.exports = (knex) => {
     });
   })
 
+
+  router.post("/:resourceid/ratings", (req, res) => {
+    const id = uuid.v4();
+    const user_id = req.session.user_id;
+    const resource_id = req.params.resourceid;
+
+    knex('ratings')
+    .select('id')
+    .where('user_id', user_id)
+    .andWhere('resource_id', resource_id)
+    .then((results) => {
+      if (!results[0]) {
+        knex('ratings')
+        .insert({
+          id: id,
+          user_id: user_id,
+          resource_id: resource_id
+        })
+        .return({inserted: true});
+      }
+    })
+  });
+
+
   return router;
 }
