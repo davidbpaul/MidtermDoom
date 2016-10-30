d554baf20bf0b2177b3cc08689142268ed649d5d
Noelle <perdue.noelle@gmail.com>
2016-10-30 20:22:53 +0000

    migrations

---
+++ db/migrations/20161027144738_update_names.js
@@ -0,0 +1,57 @@
+'use strict';
+exports.up = (knex, Promise) => {
+  return knex.schema
+  .createTable('users', (table) => {
+    table.increments('id');
+    table.string('name');
+    table.string('email');
+    table.string('password');
+  })
+  .createTable('resources', (table) => {
+    table.increments('id');
+    table.string('link');
+    table.string('title');
+    table.string('description');
+  })
+  .createTable('user_resource', (table) => {
+    table.integer('user_id');
+    table.integer('resource_id');
+  })
+  .createTable('comments', (table) => {
+    table.increments('id');
+    table.string('comment');
+    table.integer('user_id');
+    table.integer('resource_id');
+  })
+  .createTable('likes', (table) => {
+    table.increments('id');
+    table.integer('user_id');
+    table.integer('resource_id');
+  })
+  .createTable('ratings', (table) => {
+    table.increments('id');
+    table.integer('rating');
+    table.integer('user_id');
+    table.integer('resource_id');
+  })
+  .createTable('topic_resource', (table) => {
+    table.integer('topic_id');
+    table.integer('resource_id');
+  })
+  .createTable('topics', (table) => {
+    table.increments('id');
+    table.string('topic');
+  });
+};
+
+exports.down = (knex, Promise) => {
+  return knex.schema
+  .dropTable('users')
+  .dropTable('resources')
+  .dropTable('user_resource')
+  .dropTable('comments')
+  .dropTable('likes')
+  .dropTable('ratings')
+  .dropTable('topic_resource')
+  .dropTable('topics');
+};
